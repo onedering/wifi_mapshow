d9f85e2d693ed70e7d077f0ac5ca7bc60baec092
_MiXaLi4_ <91153201+onedering@users.noreply.github.com>
2026-02-10 16:32:08 +0300

    Scale points by template dimensions and dot size

--- app/src/main/java/com/example/wifi_mapshow/data/JsonRepository.kt
+++ app/src/main/java/com/example/wifi_mapshow/data/JsonRepository.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.net.Uri
 import com.google.gson.Gson
 import java.io.InputStreamReader
-import com.google.gson.annotations.SerializedName
 
 class JsonRepository(private val context: Context) {
 
@@ -12,7 +11,10 @@ class JsonRepository(private val context: Context) {
         val inputStream = context.contentResolver.openInputStream(jsonUri)
             ?: error("Cannot open JSON")
 
-        val reader = InputStreamReader(inputStream)
-        return Gson().fromJson(reader, ProjectRoot::class.java)
+        inputStream.use { stream ->
+            InputStreamReader(stream).use { reader ->
+                return Gson().fromJson(reader, ProjectRoot::class.java)
+            }
+        }
     }
 }
